637d90b8977540e8568bdda770158afe196242d4
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2024-09-05 00:37:33 +0530

    req handling#1: creating server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.status(200).send('hello !');
+});
+
+app.get('/getit', (req, res) => {
+    res.status(200).json({ message: 'hello from `/get`' });
+});
+
+app.post('/post', (req, res) => {
+    const myJSON = req.body;
+    res.status(200).json({ message: myJSON });
+});
+
+app.post('/postform', (req, res) => {
+    res.status(200).send(JSON.stringify(req.body));
+});
+
+app.listen(PORT, () => console.log(`lsitening to: http://localhost:3000`));
